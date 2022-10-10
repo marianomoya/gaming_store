f167336a25723730d9f9c83f3d2afa4a9f035be1
Mariano Moya <mariano.moya@gmail.com>
2022-10-10 17:55:34 -0300

    se agrega array de juegos y una opcion nueva menu

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 let total = 0;
-
+const gamesArray = [];
 
 class User{
     constructor(username,password){
         this.username = username;
         this.password = password;
     }
+
+
 }
 function login(){
     alert("Hola! antes de empezar tienes que crear un usuario y contraseña");
@@ -32,10 +34,33 @@ function login(){
 
 }
 
+function selectGenre(genre){
+    if(genre == 0){
+        genre = "Aventura";
+    }else if(genre == 1){
+        genre = "Accion";
+    }else if(genre == 2){
+        genre = "Shooter";
+    }else if(genre == 3){
+        genre = "Mundo Abierto";
+    }else{
+        genre = "Carreras"
+    }
+    return genre;
+};
+
 
 function addGame(){
-    let nroGames = parseInt(prompt("Cuantos juegos desea comprar?(minimo 1, maximo 20): "));
-
+    let nroGames = parseInt(prompt("Cuantos juegos desea comprar?(minimo 1, maximo 20): "))
+    
+    class Games{
+        constructor(gameName, gameGenre, gamePrice, gamePayForm){
+            this.gameName = gameName;
+            this.gameGenre = gameGenre;
+            this.gamePrice = gamePrice;
+            this.gamePayForm = gamePayForm;
+        }
+    }    
     while(nroGames<1 || nroGames>20){
         alert("Ingresaste una cantidad incorrecta, intentalo otra vez.");
         nroGames = parseInt(prompt("Cuantos juegos desea comprar?(minimo 1, maximo 20): "));
@@ -48,28 +73,40 @@ function addGame(){
             alert("No elegiste ninguna de las opciones, intenta de nuevo.");
             gameGenre = parseInt(prompt("Elija el genero del juego: \n0.Aventura \n1.Accion \n2.Shooter \n3.Mundo Abierto \n4.Carreras"));
         }
+        gameGenre = selectGenre(gameGenre);
         let price = parseFloat(prompt("Cual es el precio del juego?: "));
         let payForm= prompt("Pagas con tarjeta de debito o credito? con credito es un 10% mas");
         payForm.toLowerCase;
         if(payForm =="credito"){
             price = price *1.10;
+            Math.round(price);
         }
+        const game = new Games(gameName, gameGenre, price, payForm);
+        gamesArray.push(game);
         total = total + price;
     }
+    console.log(gamesArray);
 }
 
 const menu= ()=> {
-    let option = parseInt(prompt("Que quieres hacer?: \n0.Agregar productos al carrito \n1.Ver total de compra \n2.Salir"));
+    let option = parseInt(prompt("Que quieres hacer?: \n0.Agregar productos al carrito \n1.Ver total de compra \n2.Ver todos los productos en el carrito. \n3.Salir"));
     switch(option){
         case 0:
             addGame();
             menu();
             break;
         case 1:
-            alert("El precio total de los productos del carrito es: "+ total);
+            alert("El precio total de los productos del carrito es: "+ Math.round(total));
             menu();
             break;
         case 2:
+            let i = 1;
+            for(const game of gamesArray){
+                alert(`el juego n ${i} es ${game.gameName}, su genero es ${game.gameGenre}, su forma de pago es ${game.gamePayForm} y su precio es $${Math.round(game.gamePrice)}`);
+                i++;
+            }
+            break;
+        case 3:
             alert("Gracias por visitarnos!");
             break;
         default:
